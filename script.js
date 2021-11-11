9411562a7fff09967dec8ce504df542b446f8b4e
MAHEK <syedamahek2000@gmail.com>
2021-11-11 19:11:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src','sound.mp3');
+// audio.loop=true;
+// audio.play();
+
+//  const audio = new Audio();
+//  audio.src = "C:\Users\Welcome\Downloads\Formula 1 Car Sound.mp3";
+//  setTimeout(() => {
+//    audio.play()
+//  }, 1000);
